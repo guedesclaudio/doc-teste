3813875eaa2c8c4ee2f6288447dd46518cf490dd
Cláudio Guedes <claudio.guedes@agriness.com>
2026-03-12 17:01:07 -0300

    a

--- src/data/rules/index.ts
+++ src/data/rules/index.ts
@@ -1,14 +1,14 @@
 import type { RuleGroup } from '../../components/RulesExplorer/types';
 
 // To add a new rule group: create a JSON file in this folder and import it below.
-import animalWeaningRules from './animal_weaning.json';
 import animalLossOffRules from './animal_loss_off.json';
-import animalTransferRules from './animal_transfer.json';
-import animalParturitionRules from './animal_parturition.json';
 import animalLossRprRules from './animal_loss_rpr.json';
+import animalMatingRules from './animal_mating.json';
 import animalMissedHeatRules from './animal_missed_heat.json';
 import animalNurseMotherRules from './animal_nurse_mother.json';
-import animalMatingRules from './animal_mating.json';
+import animalParturitionRules from './animal_parturition.json';
+import animalTransferOffRules from './animal_transfer_off.json';
+import animalWeaningRules from './animal_weaning.json';
 
 export const ruleGroups: RuleGroup[] = [
   {
@@ -20,11 +20,6 @@ export const ruleGroups: RuleGroup[] = [
     group: 'animal_loss_off',
     label: 'Animal Loss Off',
     rules: animalLossOffRules,
-  },
-  {
-    group: 'animal_transfer',
-    label: 'Animal Transfer',
-    rules: animalTransferRules,
   },
   {
     group: 'animal_parturition',
@@ -51,6 +46,12 @@ export const ruleGroups: RuleGroup[] = [
     label: 'Animal Mating',
     rules: animalMatingRules,
   },
+  {
+    group: 'animal_transfer_off',
+    label: 'Animal Transfer Off',
+    rules: animalTransferOffRules,
+  },
 ];
 
 export type { RuleGroup };
+
